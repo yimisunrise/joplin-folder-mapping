5a68b3c93f118003908c4dc75dd1f6bca65d5351
chenglin <chengl20@chinatelecom.cn>
2025-03-08 17:56:59 +0800

    完成插件的配置

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import joplin from 'api';
 import { MenuItemLocation } from 'api/types';
 import { FolderUtils } from './utils';
+import * as path from 'path';
 
 joplin.plugins.register({
 	onStart: async function() {
 		// eslint-disable-next-line no-console
 		console.info('joplin folder transport plugin started!');
+
+        // 注册插件设置
+        await joplin.settings.registerSection('folderTransportSection', {
+            label: 'Folder Transport',
+            iconName: 'fas fa-folder',
+        });
+
+        await joplin.settings.registerSettings({
+            'defaultFolderPath': {
+                value: '',
+                type: 2, // String
+                section: 'folderTransportSection',
+                public: true,
+                label: '默认根路径',
+                description: '用于与Joplin笔记本路径拼接为系统完整路径',
+            },
+        });
 
         // 处理菜单项点击事件
         await joplin.commands.register({
@@ -28,13 +46,19 @@ joplin.plugins.register({
             execute: async (selectedFolder: any) => {
 				const folderId = selectedFolder ? selectedFolder.id : null;
                 if (folderId) {
+                    // 获取设置中的默认根路径
+                    const settings = await joplin.settings.values(['defaultFolderPath']);
+                    const defaultFolderPath = settings['defaultFolderPath'] as string;
+
 					// 通过目录ID获取目录路径
 					const folderPath = await FolderUtils.getFolderPath(folderId);
+
 					// 拼接系统目录路径
-					const fullFolderPath = '/home' + folderPath;
-					console.info('系统完整路径:' + fullFolderPath);
+                    const fullFolderPath = path.join(defaultFolderPath, folderPath);
+
 					// 创建系统目录如果不存在时
-					FolderUtils.createSystemFolderOfNotExist(fullFolderPath)
+					FolderUtils.createSystemFolderOfNotExist(fullFolderPath);
+
 					// 打开系统目录
 					FolderUtils.openSystemFolder(fullFolderPath);
                 } else {
